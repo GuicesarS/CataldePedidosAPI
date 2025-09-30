c804230dc130d52993196044beaeef8ff19bab4c
guicesars <guicesarsosil@gmail.com>
2025-09-30 14:37:57 -0300

    feat(application): add FluentValidation validators for CriarPedidoDto and AdicionarOcorrenciaDto

---
+++ src/backend/Catalde.Pedidos.Application/Validators/AdicionarOcorrenciaDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using Catalde.Pedidos.Application.DTOs;
+using FluentValidation;
+
+namespace Catalde.Pedidos.Application.Validators;
+
+public class AdicionarOcorrenciaDTOValidator : AbstractValidator<AdicionarOcorrenciaDTO>
+{
+    public AdicionarOcorrenciaDTOValidator()
+    {
+        RuleFor(x => x.PedidoId)
+            .GreaterThan(0)
+            .WithMessage("PedidoId deve ser maior que zero.");
+
+        RuleFor(x => x.TipoOcorrencia)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Tipo de ocorrência inválido.");
+    }
+}
---
+++ src/backend/Catalde.Pedidos.Application/Validators/CriarPeditoDTOValidator.cs
@@ -0,0 +1,14 @@
+﻿using Catalde.Pedidos.Application.DTOs;
+using FluentValidation;
+
+namespace Catalde.Pedidos.Application.Validators;
+
+public class CriarPeditoDTOValidator : AbstractValidator<CriarPedidoDTO>
+{
+    public CriarPeditoDTOValidator()
+    {
+        RuleFor(x => x.NumeroPedido)
+            .GreaterThan(0)
+            .WithMessage("Número do pedido deve ser maior que zero.");
+    }
+}
