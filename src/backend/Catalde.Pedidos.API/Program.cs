3f8fc7709f9eb7dd21bb50ed93aa23aaff1374c6
guicesars <guicesarsosil@gmail.com>
2025-09-30 20:38:20 -0300

    chore(api): configure dependency injection and database connection in Program.cs

--- src/backend/Catalde.Pedidos.API/Program.cs
+++ src/backend/Catalde.Pedidos.API/Program.cs
@@ -1,12 +1,19 @@
 using Catalde.Pedidos.Application.Services;
+using Catalde.Pedidos.Infrastructure.Context;
+using Catalde.Pedidos.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 builder.Services.AddScoped<IPedidoService, PedidoService>();
+builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
+
+builder.Services.AddDbContext<PedidoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
