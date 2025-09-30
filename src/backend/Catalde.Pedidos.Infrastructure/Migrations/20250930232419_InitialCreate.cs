ba9d23e6bd6842254f78c61870daca11fffd753d
guicesars <guicesarsosil@gmail.com>
2025-09-30 20:36:50 -0300

    chore(infrastructure): add initial EF Core migration and database schema

---
+++ src/backend/Catalde.Pedidos.Infrastructure/Migrations/20250930232419_InitialCreate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalde.Pedidos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    IdPedido = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroPedido = table.Column<int>(type: "int", nullable: false),
+                    HorarioPedido = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IndEntregue = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.IdPedido);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ocorrencias",
+                columns: table => new
+                {
+                    IdOcorrencia = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoOcorrencia = table.Column<int>(type: "int", nullable: false),
+                    HoraOcorrencia = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IndFinalizadora = table.Column<bool>(type: "bit", nullable: false),
+                    PedidoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ocorrencias", x => x.IdOcorrencia);
+                    table.ForeignKey(
+                        name: "FK_Ocorrencias_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "IdPedido",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ocorrencias_PedidoId",
+                table: "Ocorrencias",
+                column: "PedidoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ocorrencias");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
